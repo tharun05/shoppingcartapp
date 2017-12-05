8c5fead23af61aef2a5b6aca7601e79261396064
LAPTOP-MJICMDF2\Tharun <tharundillikar@gmail.com>
2017-12-05 08:53:08 +0530

    commit for folder structure

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+
+import { Component, OnInit } from '@angular/core';
+import{Ingredient} from './../shared/ingredient';
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredient : Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('burgers', 30),
+  ]
+  constructor() {
+    console.log(this.ingredient);
+   }
+
+  ngOnInit() {
+  }
+
+}
